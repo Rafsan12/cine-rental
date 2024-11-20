e97b228bb116f48c0f11b075dd6aee8032dc7e6e
Rafsan12 <rafsan.web.2022@gmail.com>
2024-11-20 08:39:31 +0600

    complete context part and cart part

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
+import { useState } from "react";
 import MovieList from "./components/Cine/MovieList";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
+import { MovieContext } from "./context";
 
 function App() {
+  const [cartData, setCartData] = useState([]);
   return (
     <>
-      <Header />
-      <main>
-        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
-          <Sidebar />
-          <MovieList />
-        </div>
-      </main>
-      <Footer />
+      <MovieContext.Provider value={{ cartData, setCartData }}>
+        <Header />
+        <main>
+          <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+            <Sidebar />
+            <MovieList />
+          </div>
+        </main>
+        <Footer />
+      </MovieContext.Provider>
     </>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,28 @@
+import { useContext, useState } from "react";
 import Moon from "../../assets/icons/moon.svg";
 import Logo from "../../assets/logo.svg";
 import Ring from "../../assets/ring.svg";
 import shoppingCart from "../../assets/shopping-cart.svg";
+import { MovieContext } from "../../context";
+import CartDetails from "../Cine/CartDetails";
 export default function Header() {
+  const [showCart, setShowCart] = useState(false);
+  const { cartData } = useContext(MovieContext);
+  console.log(cartData);
+
+  const handleCartShow = () => {
+    setShowCart(true);
+  };
+
+  const handleCartClose = () => {
+    setShowCart(false);
+  };
   return (
     <>
+      const {cartData} = useContext(MovieContext);
+      {showCart && (
+        <CartDetails onClose={handleCartClose} cartData={cartData} />
+      )}
       <header>
         <nav className="container flex items-center justify-between space-x-10 py-6">
           <a href="index.html">
@@ -30,6 +48,7 @@ export default function Header() {
             </li>
             <li>
               <a
+                onClick={handleCartShow}
                 className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
                 href="#"
               >
